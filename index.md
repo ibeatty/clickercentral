d31652adaf4062f92439cf33c215577029dfea06
ibeatty <idbeatty@uncg.edu>
2024-10-31 15:09:00 -0400

    Starting with the jekyll-starter from electricbookworks

---
+++ index.md
@@ -0,0 +1,9 @@
+---
+title: Home
+description: ""
+style: home
+---
+
+# The start of a site
+
+This is your home page. Make it nice.
---
+++ js/bundle.js
@@ -0,0 +1,7 @@
+---
+layout: null
+---
+
+{% include_relative sticky-footer.js %}
+{% include_relative tables.js %}
+{% include_relative videos.js %}
